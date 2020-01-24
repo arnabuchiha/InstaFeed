07fc3891cecbf3aa3b9cb832118ad7b1a3c08e1d
arnabuchiha <arnabkeya04@gmail.com>
2020-01-24 12:59:48 +0530

    Updates

---
+++ precache-manifest.0de8e504f1d5d405a749d281ff02b597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3070df2b52d4e5aab36466e0a6bd2b9",
+    "url": "/InstaFeed/index.html"
+  },
+  {
+    "revision": "e23fbbc2c69d87ce7be0",
+    "url": "/InstaFeed/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5a7ca148778fbb8fe587",
+    "url": "/InstaFeed/static/css/main.f3411506.chunk.css"
+  },
+  {
+    "revision": "e23fbbc2c69d87ce7be0",
+    "url": "/InstaFeed/static/js/2.76a3dce0.chunk.js"
+  },
+  {
+    "revision": "bf2ae07ce2b523ce62d6d350e531b0f9",
+    "url": "/InstaFeed/static/js/2.76a3dce0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7ca148778fbb8fe587",
+    "url": "/InstaFeed/static/js/main.a425799d.chunk.js"
+  },
+  {
+    "revision": "40836c7c9f86951a0698",
+    "url": "/InstaFeed/static/js/runtime-main.69354799.js"
+  }
+]);
